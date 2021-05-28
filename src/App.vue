6a9f643ef8c86f0ea7bab0016fd89a96ee4d343c
Орозобаев Ибрагим Жаныбекович <IOrozobaev@skymobile.local>
2021-05-28 09:44:54 +0600

    remove comment

--- src/App.vue
+++ src/App.vue
@@ -24,62 +24,59 @@ export default {
   },
   mounted() {
     M.AutoInit();
-    //       (function () {
-    //     class SlideShow {
-    //         constructor(startIndex, element) {
-    //             this.startIndex = startIndex;
-    //             this.currentIndex = this.startIndex;
-    //             this.element = element;
-    //             this.slides = this.element.querySelectorAll('.slide');
-    //             this.setActiveSlide();
-    //             this.next();
-    //             this.prev();
-    //         }
+    (function() {
+      class SlideShow {
+        constructor(startIndex, element) {
+          this.startIndex = startIndex;
+          this.currentIndex = this.startIndex;
+          this.element = element;
+          this.slides = this.element.querySelectorAll(".slide");
+          this.setActiveSlide();
+          this.next();
+          this.prev();
+        }
 
-    //         setActiveSlide() {
-    //             this.slides.forEach((item, index) => {
-    //                 if (index === this.currentIndex) {
-    //                     item.classList.add('actives');
-    //                 } else {
-    //                     item.classList.remove('actives');
-    //                 }
-    //             })
+        setActiveSlide() {
+          this.slides.forEach((item, index) => {
+            if (index === this.currentIndex) {
+              item.classList.add("actives");
+            } else {
+              item.classList.remove("actives");
+            }
+          });
+        }
 
-    //         }
+        next() {
+          let nextBtn = this.element.querySelector('[data-way="next"]');
+          nextBtn.addEventListener("click", () => {
+            if (this.currentIndex === this.slides.length - 1) {
+              this.currentIndex = 0;
+            } else {
+              this.currentIndex++;
+            }
+            this.setActiveSlide();
+          });
+        }
 
-    //         next() {
-    //             let nextBtn = this.element.querySelector('[data-way="next"]');
-    //             nextBtn.addEventListener('click', () => {
-    //                 if (this.currentIndex === this.slides.length - 1) {
-    //                     this.currentIndex = 0;
-    //                 } else {
-    //                     this.currentIndex++;
-    //                 }
-    //                 this.setActiveSlide();
-    //             })
+        prev() {
+          let prevBtn = this.element.querySelector('[data-way="prev"]');
+          prevBtn.addEventListener("click", () => {
+            if (this.currentIndex === 0) {
+              this.currentIndex = this.slides.length - 1;
+            } else {
+              this.currentIndex--;
+            }
+            this.setActiveSlide();
+          });
+        }
+      }
 
-    //         }
+      let slideShow = document.querySelectorAll(".slideshow");
 
-    //         prev() {
-    //             let prevBtn = this.element.querySelector('[data-way="prev"]');
-    //             prevBtn.addEventListener('click', () => {
-    //                 if (this.currentIndex === 0) {
-    //                     this.currentIndex = this.slides.length - 1;
-    //                 } else {
-    //                     this.currentIndex--;
-    //                 }
-    //                 this.setActiveSlide();
-    //             })
-    //         }
-    //     }
-
-    //     let slideShow = document.querySelectorAll('.slideshow');
-
-    //     slideShow.forEach(item => {
-    //         new SlideShow(0, item)
-    //     })
-
-    // }())
+      slideShow.forEach((item) => {
+        new SlideShow(0, item);
+      });
+    })();
   },
 };
 </script>
